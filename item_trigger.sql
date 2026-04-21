8a44852efcb6aedd154ecfc28a79b71b09817257
ADITYAGKARHADKAR <adityagkarhadkar@gmail.com>
2026-04-21 13:54:14 +0530

    third change

--- item_trigger.sql
+++ item_trigger.sql
@@ -6,8 +6,16 @@ CREATE TRIGGER chg_item_status
 BEFORE UPDATE ON item
 FOR EACH ROW
 BEGIN
+    -- When item changes from lost → found
     IF NEW.status = 'found' AND OLD.status = 'lost' THEN
+        
+        -- set item timestamp
         SET NEW.status_change_date = NOW();
+        UPDATE complaint
+        SET complaint_status = 'closed',
+            complaint_closure_date = NOW()
+        WHERE complaint_id = NEW.complaint_id;
+
     END IF;
 END//
 
